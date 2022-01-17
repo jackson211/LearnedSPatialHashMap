ad54f336b097f6274a79d70486bd1c111f2a422b
jackson211 <jackson21182@gmail.com>
2022-01-18 07:52:46 +0800

    save

--- src/algorithm/map.rs
+++ src/algorithm/map.rs
@@ -1,3 +1,5 @@
+use core::hash::{BuildHasher, Hash};
+use core::mem;
 use hashbrown::hash_map::DefaultHashBuilder;
 use hashbrown::raw::RawTable;
 
@@ -6,19 +8,41 @@ pub struct LearnedHashMap<K, V, H = DefaultHashBuilder> {
     table: RawTable<(K, V)>,
 }
 
-impl<K, V> LearnedHashMap<K, V, DefaultHashBuilder> {
-    pub fn new() -> Self {
-        let table = RawTable::new();
+#[cfg_attr(feature = "inline-more", inline)]
+pub(crate) fn make_insert_hash<K, S>(hash_builder: &S, val: &K) -> u64
+where
+    K: Hash,
+    S: BuildHasher,
+{
+    hash_builder.hash_one(val)
+}
+
+impl<K, V, S> LearnedHashMap<K, V, S> {
+    #[cfg_attr(feature = "inline-more", inline)]
+    pub const fn with_hasher(hash_builder: S) -> Self {
         Self {
-            hash_builder: DefaultHashBuilder::default(),
-            table,
+            hash_builder,
+            table: RawTable::new(),
         }
     }
-    pub fn with_capacity(capacity: usize) -> Self {
-        let table = RawTable::with_capacity(capacity);
+
+    #[cfg_attr(feature = "inline-more", inline)]
+    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
         Self {
-            hash_builder: DefaultHashBuilder::default(),
-            table,
+            hash_builder,
+            table: RawTable::with_capacity(capacity),
+        }
+    }
+
+    #[cfg_attr(feature = "inline-more", inline)]
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        let hash = make_insert_hash::<K, S>(&self.hash_builder, &k);
+        if let Some((_, item)) = self.table.get_mut(hash, equivalent_key(&k)) {
+            Some(mem::replace(item, v))
+        } else {
+            self.table
+                .insert(hash, (k, v), make_hasher::<K, _, V, S>(&self.hash_builder));
+            None
         }
     }
 }
